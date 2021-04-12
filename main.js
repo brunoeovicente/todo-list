2e725c1912b6b7d9113577a6d04192449b2faa61
brunoeovicente <brunoeovicente@gmail.com>
2021-04-12 18:37:32 -0300

    Revisão, estudo e tentativa de resolver os exercícios propostos pelo meu mentor

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const addTodo = (text, checked=false) => {
     renderList();
 }
 
+
 const removeTodo = (index) => {
     if (!index) {
         return;
@@ -14,22 +15,37 @@ const removeTodo = (index) => {
     todoList = todoList.filter ((todo)=>{
         if (todo.id===index) {
             return false;
+             
         }
         return true;
     });
     renderList();
 }
 
+/* Esta função, como diz o nome, trata de remover uma tarefa de acordo com seu index. Observe que cada index é único. Além disso, ela chama a função renderList abaixo, isso pois ela precisa limpar o texto que foi impresso no html */ 
+
 const setupAddTodos = () => {
     const button = document.getElementById("btn-new");
+    
+    /*
+    button.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+          event.preventDefault();
+          document.getElementById("btn-new").click();
+        }
+    */
 
     button.addEventListener ("click", () => {
         const input = document.getElementById("new-todo");
         addTodo(input.value);
+        document.getElementById("todo-content").value = "";
+        todoContent.innerHTML = "";
     })
 }
 
 setupAddTodos();
+
+/* Comunicação com o css. O botão é responsável por dar início a algumas funções do js, por efeito cascata. Note que aqui é de extrema importância a caracterização do input como um valor unico relativo a uma tarefa adicionada */
 
 const renderList = () => {
     const todoContent = document.getElementById("todo-content");
@@ -39,6 +55,8 @@ const renderList = () => {
         todoContent.appendChild(todo);
     });
 }
+
+/* Impressão do texto; interação direta com o html, no caso, o conteúdo da lista*/
 
 const newTodoItem = (text="", checked=false) => {
     const index = "todo-" + todoIndex;
@@ -61,6 +79,28 @@ const newTodoItem = (text="", checked=false) => {
     button.innerText = "X";
     button.addEventListener ("click", () => {
         removeTodo(index);
+
+        /*
+        const Remove = (answer) => {
+            alert("Deseja apagar esta tarefa?");
+            if (answer==true) {
+                removeTodo(index);
+            } else {
+                return false;
+            }
+        }
+        */
+
+        /*
+        var answer = window.confirm("Deseja apagar esta tarefa?");
+        var answer;
+        if (answer==true) {
+            removeTodo(index);
+        } else {
+            return false;
+        }
+        */
+
     });
 
     label.appendChild(input);
@@ -72,33 +112,6 @@ const newTodoItem = (text="", checked=false) => {
     
     return li;
 }
-
-/* 
-i) Limpar o input, após adicionar o todo; document.getElementById('elementid').value = ""; javascript clear a text input;
-ii) Perguntar o usuário se ele tem certeza antes de remover o todo; var answer = window.confirm("Save data?");
-if (answer) {
-    //some code
-}
-else {
-    //some code
-}
-iii) Inserir o todo ao pressionar a tecla enter.
-// Get the input field
-var input = document.getElementById("myInput");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("myBtn").click();
-  }
-});
-*/
-
-
 
 
 
